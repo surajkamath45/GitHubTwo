da2c2bf8bd5bc7c416fd468b0ae20f9d5f8d6081
Suraj <Suraj@R2D2>
2018-04-27 06:05:31 +0530

    Initial checkin

---
+++ GitHubTwo/src/packagetwo/classtwo.java
@@ -0,0 +1,17 @@
+package packagetwo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class classtwo {
+	public static void main(String[] args) {
+		List<Integer> intList= new ArrayList<Integer>();
+		
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
+		System.out.println(intList);
+		
+	}
+
+}
